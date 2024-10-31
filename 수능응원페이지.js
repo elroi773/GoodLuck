3b885320430883b0e3460c5de704b988d57a5643
김이레 <165885725+elroi773@users.noreply.github.com>
2024-10-31 19:32:42 +0900

    마지막 개발자 한마디 수정

--- 수능응원페이지.js
+++ 수능응원페이지.js
@@ -44,3 +44,39 @@ const examDate = new Date('2024-11-14T00:00:00');
             }
         }
   
+        const quotes = [
+            { text: "삶이 있는 한 희망은 있다", author: "키케로" },
+            { text: "산다는것 그것은 치열한 전투이다.", author: "로망로랑" },
+            { text: "언제나 현재에 집중할 수 있다면 행복할것이다", author: "파울로 코엘료" },
+            { text: "피할수 없으면 즐겨라", author: "로버트 엘리엇" },
+            { text: "고통이 남기고 간 뒤를 보라! 고난이 지나면 반드시 기쁨이 스며든다", author: "괴테" },
+            { text: "겨울이 오면 봄이 멀지 않으리", author: "셸리" },
+            { text: "네 자신의 불행을 생각하지 않게 되는 가장 좋은 방법은 일에 몰두하는 것이다.", author: "베토벤" },
+            { text: "문제점을 찾지 말고 해결책을 찾으라 ", author: "헨리포드" },
+            { text: "생각은 마법의 열쇠예요", author: "피터팬" },
+            { text: "가장 어두운 밤도 끝나고 해가 떠요", author: "빅터 휴고" },
+            { text: "모든 모험은 어딘가로 향하는 출발점이 필요해", author: "이상한 나라의 앨리스" },
+            { text: "마음이 얼마나 슬퍼도 계속 믿는다면 원하는 꿈은 이뤄질 거예요", author: "신데렐라" },
+            { text: "역경에서 피어나는 꽃은 가장 희귀하고 아름다워요", author: "뮬란" },
+            { text: "가끔 옳은 길은 가장 쉬운 길이 아니다", author: "그랜드 모프" },
+            { text: "당신은 믿는 것보다 용감하고 보이는 것보다 강하며, 생각하는 것 보다 똑똑해", author: "곰돌이 푸" },
+            { text: "믿음과 신뢰가 필요한 거야", author: "팅커벨" },
+            { text: "기적도 조금의 시간이 필요해요", author: "신데렐라 요정 할매" },
+            { text: "과거는 당신의 머리 속에 있고 미래는 당신의 손에 있어요 ", author: "피글렛" },
+            { text: "내게 능력 주시는 자 안에서 내가 모든 것을 할 수 있느니라", author: "빌립보서 4장 13절" },
+            { text: "그러나 내가 가는 길을 그가 아시나니 그가 나를 단련하신 후에는 내가 순금 같이 되어 나오리라", author: "욥기 23장 10절" },
+            { text: "아가야 작은 파도들 때문에 스스로가 바다임을 잊지마라", author: "익명" },
+            { text: "끝까지 달려왔으니 앞으로 18M만 더 가보자 수고했고! 축하한다!", author: "서울대 반수생" },
+            { text: "재수 없이 대학 가라!", author: "이세이세이세현" },
+            { text: "재수 없다!!", author: "나홀로집에있쥐" },
+            { text: "뭐가 걱정이야! 너를 믿어!", author: "미세스 위~" },
+            { text: "내가 노력한 만큼 마음껏 풀고 오기", author: "익명" },
+            { text: "너의 앞길이 고속도로처럼 뻥뚤리길!! ", author: "난수능안볼꺼얏" }
+
+        ];  
+        
+        function generateQuote() {
+            const randomIndex = Math.floor(Math.random() * quotes.length);
+            document.getElementById("quote").textContent = quotes[randomIndex].text;
+            document.getElementById("author").textContent = quotes[randomIndex].author;
+        }
