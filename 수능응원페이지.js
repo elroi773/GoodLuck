674dff7cfe4294b5ea2e33b4c498a81468a53460
김이레 <165885725+elroi773@users.noreply.github.com>
2024-10-31 10:01:02 +0900

    수능 응원 페이지 수정 이름 불러오기 기능 구현.

--- 수능응원페이지.js
+++ 수능응원페이지.js
@@ -14,8 +14,33 @@ const examDate = new Date('2024-11-14T00:00:00');
             document.getElementById('countdown').innerText = `D-${days}`;
         }
 
+        const countdownInterval = setInterval(updateCountdown, 1000);
 
         function flipCard(cardContainer) {
           const card = cardContainer.querySelector('.card');
           card.classList.toggle('is-flipped');
       }
+
+      const userName = localStorage.getItem("userName");
+        if (userName) {
+            document.getElementById("greeting").innerText = `${userName}님의
+            첫 도전을 응원합니다.`;
+        }
+        function checkProgress() {
+            const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+            let checkedCount = 0;
+            
+            checkboxes.forEach((checkbox) => {
+                if (checkbox.checked) checkedCount++;
+            });
+
+            const totalItems = checkboxes.length;
+            const progressMessage = document.getElementById('progressMessage');
+
+            if (checkedCount === totalItems) {
+                progressMessage.textContent = "모든 준비 완료! 이제 마음 편히 시험을 보세요!!";
+            } else {
+                progressMessage.textContent = `현재 준비 ${checkedCount} / ${totalItems} 완료`;
+            }
+        }
+  
